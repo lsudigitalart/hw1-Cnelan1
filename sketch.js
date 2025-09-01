5f39a83d3bf3d08f651c94a68936f42657c0cfe2
Cnelan1 <cnelan1@lsu.edu>
2025-09-01 12:15:43 -0500

    EllipsesHw

---
+++ sketch.js
@@ -0,0 +1,51 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background("black");
+  
+  fill ("white");
+  ellipse (434,0, 214,214);
+  ellipse (242,125,214,214);
+  ellipse(462,-20,214,214);
+  
+  fill("black");
+  ellipse(334,272,306,306);
+  ellipse(485,-34,306,306);
+  ellipse(39,138,306,306);
+  ellipse(123,-1,306,306);
+  
+  fill ("white");
+  ellipse(462,-20,214,214);
+  ellipse(369,192,68,68);
+  ellipse(141,-26,289,289);
+  ellipse(-8,144,94,94);
+  ellipse(163,330,306,306);
+  
+  fill("black")
+  ellipse(157,345,306,306);
+  ellipse(162,18,129,129);
+  ellipse(86,-1,59,59);
+  ellipse(56,60,36,28);
+  
+
+  fill ("white");
+  ellipse (136,354,289,289);
+  ellipse(168,20,94,94);
+  
+  fill("black")
+  ellipse(140,307,37,33);
+  ellipse(149,314,37,33);
+  ellipse(56,266,129,129);
+  ellipse(183,21,30,30);
+  
+  fill ("white");
+  ellipse (51,261,94,94);
+  
+  fill("black")
+  ellipse (36,253,30,30);
+  ellipse (95,225,31,27);
+  
+}
